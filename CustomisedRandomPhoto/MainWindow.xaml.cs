1b4e44726c19dccda2393df02dccadc1f44730c1
Vivien Mouret <vivien.mouret@ynov.com>
2021-06-21 23:06:50 +0200

    🚀 displaying photos with MainWindows and a class linking a loop

---
+++ CustomisedRandomPhoto/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net;
+using System.Windows;
+
+using CustomisedRandomPhoto.ViewBinding;
+
+namespace CustomisedRandomPhoto
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml.cs
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string webRequest = "https://source.unsplash.com/random";
+
+        //private readonly BindingMainWindows bindingMainWindows =
+        //    new BindingMainWindows();
+
+        public ObservableCollection<string> Images =
+            new ObservableCollection<string> { };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DisplayListImage();
+
+            //this.DataContext = bindingMainWindows;
+            observableCollection.ItemsSource = Images;
+        }
+
+        /// <summary>
+        /// Calls each time the url link
+        /// </summary>
+        /// <returns></returns>
+        private string LoadOneNewImage()
+        {
+            //response.ResponseUri.AbsoluteUri
+
+            HttpWebRequest request = WebRequest.Create(webRequest) as HttpWebRequest;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                return (string)response.ResponseUri.AbsoluteUri;
+            }
+        }
+
+        /// <summary>
+        /// Logical interaction of a loop for a list
+        /// </summary>
+        private void DisplayListImage()
+        {
+            ObservableCollection<string> imageValue =
+                new ObservableCollection<string> { };
+
+            for (int i = 0; i < 10; i++)
+            {
+                //Images.Add(new ListImages() { Uri = this.LoadOneNewImage() } );
+                Images.Add(this.LoadOneNewImage());
+            }
+
+            observableCollection.ItemsSource = Images;
+            //bindingMainWindows.ImageUri = imageValue;
+        }
+
+    }
+    public class ListImages
+    {
+        public string Uri
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CustomisedRandomPhoto/ViewBinding/BindingMainWindows.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomisedRandomPhoto.ViewBinding
+{
+    public class BindingMainWindows : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //private ObservableCollection<string> _ImageUri;
+
+        //public BindingMainWindows()
+        //{
+        //    ImageUri = _ImageUri;
+        //}
+
+        /// <summary>
+        /// First attempt at a random site string link
+        /// </summary>
+        //public ObservableCollection<string> ImageUri
+        //{
+        //    get => this._ImageUri;
+        //    set
+        //    {
+        //        if (value != this._ImageUri)
+        //        {
+        //            this._ImageUri = (ObservableCollection<string>)value;
+        //            OnPropertyChanged();
+        //        }
+        //    }
+        //}
+
+        /// <summary>
+        /// Second attempt at a random site string link
+        /// </summary>
+        public string ImageUri
+        {
+            get;
+            set;
+        }
+
+        #region INotifyPropertyChanged
+        /// <summary>
+        /// Interaction logical data change notification
+        /// </summary>
+        /// <param name="_PropertyChanged"></param>
+        protected void OnPropertyChanged([CallerMemberName] string _PropertyChanged = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(_PropertyChanged));
+        }
+        #endregion
+    }
+}
